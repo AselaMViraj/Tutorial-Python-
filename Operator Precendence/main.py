f924dff5e820eac323282d1a8d5adb6eb85ed7b3
Asela Madusanka <asela.m.viraj@gmail.com>
2022-11-27 23:50:42 +0530

    Operator Precendence

---
+++ Operator Precendence/main.py
@@ -0,0 +1,11 @@
+# order of the arithmatic operation
+# parenthesis
+# exponentiation '**'
+# multiplication or division '*' or '/'
+# addition or subtraction '+ or '-
+
+x = 10 + 3 ** 5 - 2 / 2 * 4
+print(x)
+
+x = (2 + 3) * 10 - 3
+print(x)
