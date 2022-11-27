3cd23acd0bb7311f600f73bdf49440b5b5ad8ac4
Asela Madusanka <asela.m.viraj@gmail.com>
2022-11-27 23:21:38 +0530

    String Methods In Python

---
+++ String Methods/main.py
@@ -0,0 +1,12 @@
+course = 'python for beginners'
+print(len(course))
+
+print(course.upper())
+print(course)
+
+print(course.find('o'))
+print(course.find('beginners'))
+print(course.replace('beginners', 'absolute beginners'))
+
+course = 'python for beginners'
+print('Python' in course)
