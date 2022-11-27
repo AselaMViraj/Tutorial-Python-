2ce92c31da82761f64499666d4b527ddb5aec0d0
Asela Madusanka <asela.m.viraj@gmail.com>
2022-11-28 00:06:05 +0530

    Formatted Strings

---
+++ Formatted Strings/main.py
@@ -0,0 +1,9 @@
+first_name = 'Jhon'
+last_name = 'Smith'
+message = first_name + ' [' + last_name + '] is a coder.'
+print(message)
+
+first_name = 'Jhon'
+last_name = 'Smith'
+message = f'{first_name} [{last_name}] is a coder.'
+print(message)
