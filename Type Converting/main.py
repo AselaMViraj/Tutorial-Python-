ef23e44e92ab20f543bd9428ae7729324eadec05
Asela Madusanka <asela.m.viraj@gmail.com>
2022-11-27 23:18:54 +0530

    Type Converting

---
+++ Type Converting/main.py
@@ -0,0 +1,6 @@
+birth_year = input('Birath year: ')
+age = 2022 - int(birth_year)
+print(age)
+
+weight = input('Type your weight(in pounds): ')
+print('Your weight is(in kilograms):', (float(weight)/2.20462))
