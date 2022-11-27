8bd7358105d5c3cada3a949c53fd7a7b20b4daf5
Asela Madusanka <asela.m.viraj@gmail.com>
2022-11-27 23:20:23 +0530

    Strings In Python

---
+++ Strings/main.py
@@ -0,0 +1,31 @@
+course = 'Python for beginners'
+print(course)
+#################################
+course = "Python's for beginners"
+print(course)
+#################################
+course = 'Python for "beginners"'
+print(course)
+#################################
+course = '''Hi Asela,
+
+This is the first email.
+
+Thank you.
+
+'''
+print(course)
+#################################
+
+course = 'Python for beginners'
+print(course[-1])
+
+course = 'Python for beginners'
+print(course[0:4])
+
+course = 'Python for beginners'
+another = course[:]
+print(another)
+
+name = 'Jennifer'
+print(name[1:-1])
