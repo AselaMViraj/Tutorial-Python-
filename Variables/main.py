c9ea13d9d94f9523a8aaccc3b6afa9ac181bdb15
Asela Madusanka <asela.m.viraj@gmail.com>
2022-11-27 23:15:49 +0530

    Python Variables

---
+++ Variables/main.py
@@ -0,0 +1,20 @@
+price = 10
+print(price)
+
+price = 10 # integer
+floating = 4.9 # floating numbers
+name = 'Asela' # string
+is_available = True # boolean
+
+print(price)
+print(floating)
+print(name)
+print(is_available)
+
+name = 'Jhon Smith'
+age = 20
+is_new = True
+
+print('Full Name: ', name)
+print('Age: ', age)
+print('New Patient: ', is_new)
