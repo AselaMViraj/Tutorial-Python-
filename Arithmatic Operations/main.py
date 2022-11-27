0456dd9dce96fe16baeafcb8fa6d12d6b5d3d995
Asela Madusanka <asela.m.viraj@gmail.com>
2022-11-27 23:49:35 +0530

    Arithmatic Operations

---
+++ Arithmatic Operations/main.py
@@ -0,0 +1,15 @@
+print(10+3) # addition
+print(10-3) # subtraction
+print(10*3) # multiplication
+print(10/3) # division with floating points
+print(10//3) # division without floating points
+print(10%3) # modulus
+print(10**3) # power
+
+x = 3
+x += 10 # augmented assignment operator
+print(x)
+
+x = 10
+x -= 3
+print(x)
