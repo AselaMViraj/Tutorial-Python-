d4fefc11a720ae40c991e7e2e467f329d90ea486
Asela Madusanka <asela.m.viraj@gmail.com>
2022-12-01 11:27:45 +0530

    Tuples In Python

---
+++ Tuples/main.py
@@ -0,0 +1,11 @@
+# accessing any item in tuple
+numbers = (1, 2, 3)
+print(numbers[0])
+
+# count any item
+numbers = (1, 2, 3)
+print(numbers.count(1))
+
+# get the first index of item
+numbers = (1, 2, 3)
+print(numbers.index(2))
