7f91c41ad42c4ce3987bdbf3d4697188d5af3cdb
Asela Madusanka <asela.m.viraj@gmail.com>
2022-12-01 11:31:54 +0530

    Simple Example With Dictionaries(Emogi Converter)

---
+++ Emoji_Converter/main.py
@@ -0,0 +1,10 @@
+message = input(">")
+words = message.split()
+emojies = {
+    ":)": "🤗",
+    ":(": "☹"
+}
+output = ""
+for word in words:
+    output += emojies.get(word, word) + " "
+print(output)
