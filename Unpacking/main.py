e4deabe41453d0232255e1d3d70155a579156639
Asela Madusanka <asela.m.viraj@gmail.com>
2022-12-01 11:28:44 +0530

    Unpacking

---
+++ Unpacking/main.py
@@ -0,0 +1,18 @@
+# unpacking in tuples
+import coordinates as coordinates
+
+coordinates = (1, 2, 3)
+x, y, z = coordinates
+'''x = coordinates[0}
+y = coordinates[1]
+z = coordinates[2]'''
+print(x)
+print(y)
+print(z)
+
+# unpacking in lists
+coordinates = [1, 2, 3]
+x, y, z = coordinates
+print(x)
+print(y)
+print(z)
