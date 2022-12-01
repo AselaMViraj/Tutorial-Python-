e9c8021795bcdb83892b4cdb7498c938fd16a62b
Asela Madusanka <asela.m.viraj@gmail.com>
2022-12-01 11:37:28 +0530

    Organizing Program (Modules)

---
+++ Modules/Converters.py
@@ -0,0 +1,6 @@
+def kilo_to_pound(weight):
+    return weight/0.45
+
+
+def pound_kilo(weight):
+    return weight*0.45
---
+++ Modules/main.py
@@ -0,0 +1,9 @@
+# first method
+import Converters
+
+print(Converters.pound_kilo(100))
+
+# second method
+from Converters import pound_kilo
+
+print(pound_kilo(100))
